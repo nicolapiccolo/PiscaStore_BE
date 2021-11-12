11a2b11b66cd75944a55208dfec500317241b200
Nicola Piccolo <nikola9piccolo9@gmail.com>
2021-11-12 09:52:57 +0100

    Empty Spring Project
    Added empty project of Spring Boot

---
+++ pisca/src/main/java/it/unito/pisca/PiscaApplication.java
@@ -0,0 +1,13 @@
+package it.unito.pisca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiscaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiscaApplication.class, args);
+	}
+
+}
---
+++ pisca/src/test/java/it/unito/pisca/PiscaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unito.pisca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiscaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
