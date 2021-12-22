5fde3ba8d8417dd9adf907a077aaf702ddc0c239
Nicola Piccolo <nikola9piccolo9@gmail.com>
2021-12-22 11:55:50 +0100

    api gatewat added

---
+++ api_gateway/src/main/java/it/unito/apigatewayservice/ApigatewayserviceApplication.java
@@ -0,0 +1,36 @@
+package it.unito.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import it.unito.apigatewayservice.filters.ErrorFilter;
+import it.unito.apigatewayservice.filters.PostFilter;
+import it.unito.apigatewayservice.filters.PreFilter;
+import it.unito.apigatewayservice.filters.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApigatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayserviceApplication.class, args);
+	}
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
